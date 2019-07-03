2aecd0e7b623dffefea889525ba31a55daf45184
LeeJeYoung <jeyounglee27@gmail.com>
2019-07-03 16:40:51 +0900

    불필요한 디자인 삭제하고 pic 추가함

---
+++ app/src/main/java/com/example/swusemiproject2019/AboutMemberActivity.java
@@ -0,0 +1,13 @@
+package com.example.swusemiproject2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_member);
+    }
+}
---
+++ app/src/main/java/com/example/swusemiproject2019/MakeMemoActivity.java
@@ -0,0 +1,13 @@
+package com.example.swusemiproject2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeMemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_memo);
+    }
+}
